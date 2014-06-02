16b79f1b6ff441176d5450bdfff22810f0c021fc
kujenga <ataylor0123@gmail.com>
2014-06-02 10:28:48 -0400

    Added custom TableViewCells for specific use cases

---
+++ Peck/PAPeckCell.m
@@ -0,0 +1,25 @@
+//
+//  PAPeckCell.m
+//  Peck
+//
+//  Created by Aaron Taylor on 6/2/14.
+//  Copyright (c) 2014 Peck. All rights reserved.
+//
+
+#import "PAPeckCell.h"
+
+@implementation PAPeckCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
