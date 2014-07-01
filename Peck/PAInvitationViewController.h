591569cf90048eef8c24b267fba95651b4ce2bef
jkarabinos <jkarabinos@middlebury.edu>
2014-07-01 13:58:04 -0400

    minor changes

---
+++ Peck/PAInvitationViewController.h
@@ -0,0 +1,13 @@
+//
+//  PAInvitationViewController.h
+//  Peck
+//
+//  Created by John Karabinos on 7/1/14.
+//  Copyright (c) 2014 Peck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PAInvitationViewController : UIViewController
+
+@end
