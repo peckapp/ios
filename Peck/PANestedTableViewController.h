8b09661cce3b5714803f0248c3978e5fc71f3cb3
wurflen <jonaskluebbers@gmail.com>
2014-08-04 14:43:00 -0400

    Added custom nested table view controller

---
+++ Peck/PANestedTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PANestedTableViewController.h
+//  Peck
+//
+//  Created by Jonas Luebbers on 8/4/14.
+//  Copyright (c) 2014 Peck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PANestedTableViewController : UITableViewController
+
+@end
