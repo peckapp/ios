dc53e756c5038a65ab855802371219603267ac34
jkarabinos <jkarabinos@middlebury.edu>
2014-08-13 13:53:23 -0400

    fixed a problem where logging out while a circle cell was expanded would result in the new circle cell being expanded for the new anonymous user

--- Peck/PAProfileTableViewController2.m
+++ Peck/PAProfileTableViewController2.m
@@ -124,12 +124,24 @@ BOOL loggedIn;
 - (IBAction)login:(id)sender
 {
     if(!loggedIn){
+        
         UIStoryboard *loginStoryboard = [UIStoryboard storyboardWithName:@"Login" bundle:nil];
         UINavigationController *loginRoot = [loginStoryboard instantiateInitialViewController];
         PAInitialViewController* root = loginRoot.viewControllers[0];
         root.justOpenedApp=NO;
         [self presentViewController:loginRoot animated:YES completion:nil];
     }else{
+        //Logging out the user
+        
+        
+        PAAppDelegate *appdelegate = [[UIApplication sharedApplication] delegate];
+        if(appdelegate.circleViewController.selectedIndexPath){
+            //if there is a currently expanded cell, we must condense this cell before logging out
+            PACircleCell* cell = (PACircleCell*)[appdelegate.circleViewController.tableView cellForRowAtIndexPath:appdelegate.circleViewController.selectedIndexPath];
+            
+            [appdelegate.circleViewController condenseCircleCell:cell atIndexPath:appdelegate.circleViewController.selectedIndexPath];
+        }
+
         
         [[PASyncManager globalSyncManager] logoutUser];
         
@@ -141,6 +153,7 @@ BOOL loggedIn;
             // The session state handler (in the app delegate) will be called automatically
         [FBSession.activeSession closeAndClearTokenInformation];
         }
+        
         
         NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
         [defaults removeObjectForKey:@"authentication_token"];
