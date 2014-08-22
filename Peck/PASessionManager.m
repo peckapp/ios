256e6de96d7373f697767f45d91ffb4f2acae364
jkarabinos <jkarabinos@middlebury.edu>
2014-08-22 10:11:06 -0400

    Switched the server back to the development server.

--- Peck/PASessionManager.m
+++ Peck/PASessionManager.m
@@ -23,7 +23,7 @@ static NSString * const PAProdAPIBaseURLString = @"https://yggdrasil.peckapp.com
     static PASessionManager *_sharedClient = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        _sharedClient = [[PASessionManager alloc] initWithBaseURL:[NSURL URLWithString:PALocalAPIBaseURLString]];
+        _sharedClient = [[PASessionManager alloc] initWithBaseURL:[NSURL URLWithString:PADevAPIBaseURLString]];
         
         _sharedClient.requestSerializer = [AFJSONRequestSerializer serializer];
         [_sharedClient.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Accept"];
